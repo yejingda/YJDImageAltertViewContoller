cb87e785dd880065f0a53f66cc7b6a27f7524505
yejingda <yejingda@aipai.com>
2015-11-25 11:37:07 +0800

    Initial Commit

---
+++ YJDImageViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJDImageViewController
+//
+//  Created by yejingda on 15/11/25.
+//  Copyright © 2015年 xxxx.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJDImageViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJDImageViewController
+//
+//  Created by yejingda on 15/11/25.
+//  Copyright © 2015年 xxxx.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
